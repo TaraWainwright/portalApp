abf6ed157cc72f19bba3d8d23e71e6a53e61db3c
TaraWainwright <108867550+TaraWainwright@users.noreply.github.com>
2024-01-15 11:39:33 +0000

    Update PortalAppCode.R
    Remove notes to myself

--- PortalAppCode.R
+++ PortalAppCode.R
@@ -7,7 +7,6 @@ setwd("C:/Users/taraw/OneDrive - Natural History Museum/Documents/Data Accelerat
 
 library(tidyverse)
 library(lubridate)
-library(zoo)
 
 # Download data
 queries <- read.csv("query_doi.csv")
@@ -19,11 +18,6 @@ resourcenames <- read.csv("resources.csv")
 joined_queries <- left_join(actions, queries, by ="doi")
 joined_queries$date <- substr(joined_queries$timestamp.y, 1, 10)
 str(joined_queries, max.level = 1)
-
-
-# Extract complicated row with multiple resources in same row for testing
-# dodgy <- joined_queries[19358, ]
-# write.csv(dodgy, file = "complicated_row.csv", row.names = F)
 
 
 # Select columns of interest
@@ -38,40 +32,20 @@ narrowed_d$date <- as.Date(narrowed_d$date, format = "%Y-%m-%d")
 narrowed_d <- narrowed_d %>% 
   mutate(floored_date = floor_date(date, "month"))
 
-# narrowed_d <- narrowed_d %>% 
-#   mutate(sdate = zoo::as.yearmon(date))
-# str(narrowed_d)
-
 
 # Separate rows and extract resources into new row 'resource_id'
 resources_data <- narrowed_d %>% 
   separate_longer_delim(resources_and_versions, delim = ',') %>%
   mutate(resource_id = str_extract(resources_and_versions, '"(.*)"', 1))
-# separate_longer_delim separates row into multiple rows based on delimiter
-# mutate adds new column
 # str_extract extracts elements of column based on following regex
 # '"(.*)"' looks for any number/character within quotation marks
 # '1' (or group = 1) indicates group within regex - so extracts the group we want to extract
-
-# Another way to extract string in quotation
-# library(stringi) 
-# resources_data[ , 'resource_id'] <- NA
-# resources_data$resource_id <- stringi::stri_extract_all_regex(resources_data$resources_and_versions, '(?<=").*?(?=")')
 
 
 # Extract queries - first 'q' queries
 querydata <- resources_data %>% 
   mutate(searches = str_extract(query, '"(?:search|q)": "([^"]+)"', group = 1))
 # could also use group = 2 and get rid of the ?: bit
-
-
-# extracts string after q": " and excludes strings after comma: '"q": "(.+)"(, )*"', group = 1
-# extracts string between quotations but includes filer section: '(?<=q": ").+(?=")'
-# extracts string after colon: '(?<=:).+' e.g. "platichthys"}
-# extracts the first bit, want to do the opposite: "^[^\\:]+" e.g. {"q"
-# extracts last string: '\\S+$' 
-# extracts everything after first word: '\\b"(.+)"' e.g., ": "platichthys"
-
 
 
 # Join data by resource id
@@ -99,29 +73,8 @@ qs$searches <- gsub("[[:punct:][:blank:]]+", " ", qs$searches)
 # Remove capitalisation
 qs$searches <- tolower(qs$searches)
 
-qs2 <- df %>% 
-  group_by(resource_id, floored_date) %>%
-  select(resource_id, resource_name, floored_date, searches) %>% 
-  # create count for each word within each resource
-  add_count(searches) %>% 
-  distinct() %>% 
-  drop_na(searches)
-
-
-# qs2 <- df %>% 
-#   group_by(resource_id, floored_date) %>%
-#   select(resource_id, resource_name, floored_date, searches) %>% 
-#   # create count for each word within each resource
-#   mutate(wordcount = str_count(searches)) %>% 
-# # only keep unique rows (remove duplicates)
-#   distinct() %>% 
-#   drop_na(wordcount)
-
-
 # Save search data
 write_csv(qs, "search_data.csv")
-write_csv(qs2, "search_data_full.csv")
-
 
 # Count data: 
 
